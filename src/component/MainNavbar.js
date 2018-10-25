91536649679158b6ec7b9b2483f17df3bde51c8d
Roger <rogge_acdc@hotmail.com>
2018-10-25 14:17:29 +0200

    new Home welcometext

--- src/component/MainNavbar.js
+++ src/component/MainNavbar.js
@@ -1,52 +1,44 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-// import {
-//     Collapse,
-//     Navbar,
-//     NavbarToggler,
-//     NavbarBrand,
-//     Nav,
-//     NavItem,
-//     NavLink,
-//     Container,
-//     Row,
-//     Col,
-//     Jumbotron,
-//     Button
-// } from 'reactstrap';
+ import {
+     Collapse,
+     Navbar,
+     NavbarToggler,
+     NavbarBrand,
+     Nav,
+     NavItem,
+     NavLink,
+     Container,
+     Row,
+     Col,
+     Jumbotron,
+     Button
+ } from 'reactstrap';
 
 const MainNavbar = () => (
 
-    
+    <div>    
+        <Navbar color="light" light expand="md">
+        <NavbarBrand to="/">
         
+        React Warp Core
+        </NavbarBrand>
         
+        <Nav className="ml-auto" navbar>
+            <NavItem>
+             <Link exact to="/" activeClassName = "active">Welcome</Link>  {/*   Then changed to NavLink it dont work  */}
+            </NavItem>
+            <NavItem>
+            <Link to="/about" activeClassName = "active">About</Link>
+            </NavItem>
             
-            <Link to="/about">About</Link>
+        </Nav>
         
-        
-  /* <Navbar color="light" light expand="md">  
-    {/* <NavbarToggler onClick={e => this.toggle()} /> */
-    /*<Collapse isOpen={this.isOpen} navbar>*/
-      /* <Nav className="ml-auto" navbar>
-        <NavItem>
-          <Link exact to="/" activeClassName = "active">Welcome</Link>
-        </NavItem>
-        <NavItem>
-          <Link to="/about" activeClassName = "active">Clock</Link>
-        </NavItem>
-        <NavItem>
-          <NavLink to="/todo-list" activeClassName = "active">Todo list</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink to="/conditionals" activeClassName = "active">If-else</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink to="/loops" activeClassName = "active">Loops</NavLink>
-        </NavItem>
-      </Nav>
-    
-  </Navbar> */
+      </Navbar>
+    </div>
+  
   
 
 )
