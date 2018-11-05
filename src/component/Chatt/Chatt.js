84cc56b28b343257c246f60916d5200c1bf39f8f
Roger <rogge_acdc@hotmail.com>
2018-11-05 23:13:25 +0100

    emoji added wip

--- src/component/Chatt/Chatt.js
+++ src/component/Chatt/Chatt.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Container, Row, Col,ListGroup, ListGroupItem, Badge } from 'reactstrap';
 import openSocket from 'socket.io-client';
+import EmojiPicker from 'emoji-picker-react';
+import 'emoji-picker-react/dist/universal/style.scss';
 //import sand from './sand.jpg'
 import './Chatt.scss';
 
@@ -43,30 +45,33 @@ class Chatt extends Component {
     
       return (
         <div className="Chatt-container">
-      <Container>
-        {/* <img src={sand} alt="sand"/> */}
-        <Row className=" fixed-bottom p-4">
-          {/* <Col xs="9" className="Grushogen"> */}
         
+      <Container>
+        <Row className=" fixed-bottom p-4">  
       <div className="messages">
-      {this.state.messages.map((message, i) => <div key={i}>{message}</div>)}
       
-      <input id="m" autocomplete="off" value={this.state.message}  onKeyPress={e => e.key === 'Enter' && this.send()} onChange={e => this.changeMessage(e)} /><button className="Sendbutton" onClick={e => this.send()}>Send</button>
+      {this.state.messages.map((message, i) => <div key={i}>{message}</div>)}</div>
+      <div className="InputAndButton">
+      <input id="m" autocomplete="off" value={this.state.message}  onKeyPress={e => e.key === 'Enter' && this.send()} onChange={e => this.changeMessage(e)} />
+      <button className="Sendbutton" onClick={e => this.send()}>Send</button>
       </div>
+      </Row>
           {/* </Col> */}
-          <Col xs="3">
+         
+        
+        <Row>
+         <Col className="d-flex justify-content-end mt-5">
           <ListGroup>
+          <ListGroupItem>Aktiva Chattar</ListGroupItem>
         <ListGroupItem>Roger och Kalle <Badge pill>14</Badge></ListGroupItem>
         <ListGroupItem>Roger och Charlie <Badge pill>2</Badge></ListGroupItem>
         <ListGroupItem>Roger och Fredrik <Badge pill>1</Badge></ListGroupItem>
       </ListGroup>
-
+      </Col> </Row><Row><Col className="d-flex justify-content-end mt-5">
+<EmojiPicker preload/>
       </Col>
-        </Row>
-        <Row>
-        
-      
-        </Row>
+      </Row>
+       
       </Container>
      </div>
     );
